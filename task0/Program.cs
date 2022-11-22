72e00c9f5adb8623e90db3e58ae31a0cf23e530e
ognevok97 <ognevok97@mail.ru>
2022-11-22 18:48:07 +0700

    homework1

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,1 +1,10 @@
-﻿ReadOnlyMemory
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int num = new int();
+Console.WriteLine("Введи трехзначное число и я покажу вторую цифру этого числа");
+    num = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(num);
+Console.WriteLine("вторая цифра этого числа -"+stringNumber[1]);
